f743bcd9b54abc642fd0ab14f47f30c3e2b28bf0
Engieeeboiz <generososantiaguel4@gmail.com>
2024-04-02 09:51:38 +0800

    added file

---
+++ signup.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="signupStyle.css">
+    <title> Sign Up </title>
+</head>
+<body>
+    <div>
+
+    </div>
+    <div>
+        <form action="">
+            <ul></ul>
+        </form>
+    </div>
+    
+</body>
+</html>
